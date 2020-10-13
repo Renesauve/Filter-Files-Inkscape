063d590f158231ccc3564b6f357d298bb1bbee41
Rene <rdasauve@gmail.com>
2020-10-12 17:09:10 -0700

    Completed bot

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
 const fs = require("fs");
 const {shell, screen, globalShortcut} = require("electron");
 const robot = require("robotjs");
+
 const currentPath = "C:/Users/rdasa/Desktop/coins";
 
 fs.readdir(currentPath, {encoding: "utf8", withFileTypes: true}, function (
   err,
   files
 ) {
-  const clicky = globalShortcut.register("V", () => {
-    robot.mouseClick();
-    robot.keyTap("control");
-    robot.keyTap("shift");
-    robot.keyTap("R");
-  });
-  Promise.all(
-    files.map(async (file, index) => {
-      if (file.isFile() && clicky == true) {
+  const handleFile = async (i = 0) => {
+    const file = !!files && files?.[i];
+
+    if (file) {
+      const loadingFile = await new Promise((resolve, reject) => {
+        globalShortcut.register("V", async () => {
+          robot.mouseClick();
+          robot.keyTap("R", ["control", "shift"]);
+          robot.keyTap("S", "control");
+          robot.keyTap("Q", "control");
+          resolve(true);
+        });
+
         shell.openPath(currentPath + "/" + file.name + "/");
+      });
+      globalShortcut.unregisterAll();
+      if (i < files.length - 1) {
+        handleFile(i + 1);
       }
-    })
-  ).catch((error) => console.log({error}));
+    }
+  };
+  handleFile();
 });
